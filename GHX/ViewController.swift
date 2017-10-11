070489110a30040aa48edc8f2871c755c4dfc5b5
Michel B <michelbh@gol.com>
2017-10-11 18:45:57 +0900

    Initial Commit

---
+++ GHX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GHX
+//
+//  Created by Michel Bouchet on 2017/10/11.
+//  Copyright © 2017 Michel Bouchet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
